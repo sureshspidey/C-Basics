798fc921f2ee92ea3b0576aa52a7aca67d3c7104
sureshspidey <92290570+sureshspidey@users.noreply.github.com>
2021-11-02 11:51:32 +0530

    Add files via upload

---
+++ arithmetic opertions.c
@@ -0,0 +1,15 @@
+#include<stdio.h> 
+void main()
+{
+	int a,b,A,S,M,D;
+	a=8;
+	b=2;
+	A=a+b;
+	S=a-b;
+	M=a*b;
+	D=a/b;
+	printf("addiiton of %d and %d is %d\n",a,b,A);
+	printf ("subtraction of %d and %d is %d\n",a,b,A);
+	printf("Multilication of %d and %d is %d\n",a,b,M);
+	printf("division of %d and %d is %d\n",a,b,D);
+}
