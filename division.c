52f5d30c94745cacbafdf373346b37c573ec38af
sureshspidey <92290570+sureshspidey@users.noreply.github.com>
2021-11-02 12:00:49 +0530

    Update division.c

--- division.c
+++ division.c
@@ -2,9 +2,9 @@
 #include<stdio.h> 
 void main()
 {
-	int a,b,d;
+	float a,b,d;
 	a=8;
 	b=2;
 	d=a/b;
-	printf ("subtraction of %d and %d is %d",a,b,d);
+	printf ("subtraction of %f and %f is %f",a,b,d);
 }
