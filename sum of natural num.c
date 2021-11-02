755d4dff873b3612e90a208d19148173ae26e228
sureshspidey <92290570+sureshspidey@users.noreply.github.com>
2021-11-02 12:25:10 +0530

    Add files via upload

---
+++ sum of natural num.c
@@ -0,0 +1,14 @@
+/* sum of first n natural numbers*/
+#include<stdio.h>
+void main()
+{
+	int n,S;
+	/*S=sum of first n numbers
+	n=number of numbers*/
+    n=10;
+	S=n*(n+1)/2;
+	printf("\nsum of first %d natural number is %d",n,S);
+	
+	
+	
+}
