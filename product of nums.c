7a3c44902d980759c14d710fe3ce16c304704170
sureshspidey <92290570+sureshspidey@users.noreply.github.com>
2021-11-09 11:54:38 +0530

    Add files via upload

---
+++ product of nums.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+	int a,b,c,d,T;
+	printf("enter the 1st number  ");
+	scanf("  %d",&a);
+	printf("enter the 2nd number  ");
+	scanf("  %d",&b);
+	printf("enter the 3rd number  ");
+	scanf("  %d",&c);
+	printf("enter the 4th number  ");
+	scanf("  %d",&d);
+	T=a*b*c*d;
+	printf("product of %d,%d,%d and %d is %d",a,b,c,d,T);		
+}
+
