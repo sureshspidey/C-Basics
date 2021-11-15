ecdf41ea033f0a0e9705933d49b17a96c705ca1d
sureshspidey <92290570+sureshspidey@users.noreply.github.com>
2021-11-15 11:49:56 +0530

    Add files via upload

---
+++ num to char2.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+void main()
+{
+	int c,a,t;
+	printf("Enter the numbers:-");
+	scanf("%d %d %d",&c,&a,&t);
+	printf("\ndecoded numbers of %d,%d and %d is %c%c%c",c,a,t,95+c,95+a,95+t);
+}
