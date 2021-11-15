23f5226ac4f46132401d310c21187ac576ddb735
sureshspidey <92290570+sureshspidey@users.noreply.github.com>
2021-11-15 12:22:20 +0530

    Update char to num.c

--- char to num.c
+++ char to num.c
@@ -4,5 +4,5 @@ void main()
 	char a,b,c;
 	printf("Enter the characters:-");
 	scanf("%c %c %c",&a,&b,&c);
-	printf("\nDecoded characters of %c,%c and %c is %d,%d and %d",a,b,c,a,b,c);
+	printf("\nDecoded characters of %c,%c and %c is %d,%d and %d",a,b,c,a-95,b-95,c-95);
 }
