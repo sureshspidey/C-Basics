b72cf724045b03325b3b99c6e4dce7e3a19586c2
sureshspidey <92290570+sureshspidey@users.noreply.github.com>
2021-11-15 12:19:56 +0530

    Add files via upload

---
+++ char to num.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+void main()
+{
+	char a,b,c;
+	printf("Enter the characters:-");
+	scanf("%c %c %c",&a,&b,&c);
+	printf("\nDecoded characters of %c,%c and %c is %d,%d and %d",a,b,c,a,b,c);
+}
