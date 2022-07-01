bf85c046334d741ab4bc25d2a0c4356dbe110934
sureshspidey <92290570+sureshspidey@users.noreply.github.com>
2022-07-01 12:00:07 +0530

    Add files via upload

---
+++ arreveneven.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+	int n,esum=0,osum=0;
+	scanf("%d",&n);
+	int arr[n];
+	int i;
+	for(i=0;i<n;i++)
+	{
+		scanf("%d",&arr[i]);
+	}
+	for(i=0;i<n;i++)
+	{
+		if (i%2==0)
+		{
+			if(arr[i]%2==0)
+			{
+			printf("%d",arr[i]);	
+			}
+	    }
+	}
+	
+}
