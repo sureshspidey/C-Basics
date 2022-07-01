f4c6eb7e08a3bbc69fb9ad0bfc446a97977ece9e
sureshspidey <92290570+sureshspidey@users.noreply.github.com>
2022-07-01 11:58:39 +0530

    Add files via upload

---
+++ arrbig.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+	int n,i,k;
+	scanf("%d",&n);
+	int arr[n];
+	for(i=0;i<n;i++)
+	{
+		scanf("%d",&arr[i]);
+	}
+	for(i=0;i<n;i++);
+	{
+		if(arr[0]<arr[i])
+		{
+			
+			printf("%d",arr[]);
+		}
+	}
+}
